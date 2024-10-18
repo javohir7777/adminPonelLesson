785924e06791caa547d277cbbfa31c608f78fca4
javohir7777 <javohirjumayev7777@gmail.com>
2024-10-18 03:25:23 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// aside
+
+const sidebar = document.querySelector("nav");
+const sidebarToggle = document.querySelector(".sidebar-toggle");
+
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+if (innerWidth <= 1000) {
+  sidebar.classList.add("close");
+} else {
+  sidebar.classList.remove("close");
+}
+
+// dark-light
+
+const body = document.body;
+const modeToggle = document.querySelector("#btnMode");
+
+modeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
